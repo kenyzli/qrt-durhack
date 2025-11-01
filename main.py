e51a4f56165a94505d5bfe1b542f1a2b7b8095c3
Bart Weil <b64weil@protonmail.com>
2025-11-01 20:31:34 +0000

    feat: sort flight score by CO2 per capita

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ OFFICES = [
     "LHR"
 ]
 
-emissions_file = "/opt/durhack/emissions.csv"
+emissions_file = "emissions.csv"#"/opt/durhack/emissions.csv"
 emissions = pl.scan_csv(emissions_file, infer_schema_length=0).collect()
 # print(emissions.columns)
 
 # from A to B on this date. 
 # A and B are airport codes like LHR and BOM
 def get_flights_score(A: str, B: str, year: int, month: int, day: int) -> pl.DataFrame:
-    schedule_file = f"/opt/durhack/schedules/{year}/{month}/{day}.csv"
+    schedule_file = "durhack-2025-01/01.csv"#f"/opt/durhack/schedules/{year}/{month}/{day}.csv"
     
     schedules = pl.scan_csv(schedule_file, infer_schema_length=10000)
     emissions = pl.scan_csv(emissions_file, infer_schema_length=10000)
+    #
+    print("S")
+    print(pl.DataFrame.collect_schema(schedules)) # TOTAL_SEATS - total seats column
+    print("E")
+    print(pl.DataFrame.collect_schema(emissions)) # ESTIMATED_CO2_TOTAL_TONNES
     
     # information on the columns in the data files can be found at:
     # https://knowledge.oag.com/docs/wdf-record-layout
@@ -37,7 +42,11 @@ def get_flights_score(A: str, B: str, year: int, month: int, day: int) -> pl.Dat
             right_on=["CARRIER_CODE", "FLIGHT_NUMBER"],
             how="inner"
         )
-        .sort("ESTIMATED_CO2_TOTAL_TONNES")
+        .with_columns(
+            (pl.col("ESTIMATED_CO2_TOTAL_TONNES") / pl.col("TOTAL_SEATS").cast(pl.Float64))
+            .alias("ESTIMATED_CO2_PER_CAPITA")
+        )
+        .sort("ESTIMATED_CO2_PER_CAPITA")
         .select([
             pl.col("FLTNO"), 
             pl.col("DEPCITY"),
@@ -50,6 +59,7 @@ def get_flights_score(A: str, B: str, year: int, month: int, day: int) -> pl.Dat
             pl.col("DEPTIM"),
             pl.col("ELPTIM"),
             pl.col("ESTIMATED_CO2_TOTAL_TONNES"),
+            pl.col("ESTIMATED_CO2_PER_CAPITA"),
             pl.col("INTAPT")
         ])
     ).collect()
